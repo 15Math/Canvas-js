b49e51b0ad84520205ebfae8ca95106aeee6dbdf
15Math <matheus.loureiro1215@gmail.com>
2024-08-16 15:54:07 -0300

    canvas drawing done

--- canvas-js/index.js
+++ canvas-js/index.js
@@ -1,23 +1,41 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext("2d");
+document.addEventListener('DOMContentLoaded', ()=>{
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext("2d");
 
-let isMouseDown = false;
+    canvas.width = 800;
+    canvas.height = 500;
 
-document.addEventListener("mousedown", ()=>{
-    isMouseDown = true;
+    let brush = {
+        active: false,
+        move:false,
+        position:{x: 0, y:0},
+        lastPosition: null
+    }
+    ctx.lineWidth = 7;
+
+    const draw = (line) => {
+        ctx.beginPath();
+        ctx.moveTo(line.inicial.x, line.inicial.y);
+        ctx.lineTo(line.final.x, line.final.y);
+        ctx.stroke();
+    }
+
+    canvas.onmousedown = ()=>{brush.active = true};
+    canvas.onmouseup = ()=>{brush.active = false};
+
+    canvas.onmousemove = (event)=>{
+        brush.position.x = event.offsetX;
+        brush.position.y = event.offsetY;
+        brush.move = true;
+    };
+
+    const cicle = ()=>{
+        if(brush.active && brush.move && brush.lastPosition){
+            draw({inicial:{x:brush.lastPosition.x,y:brush.lastPosition.y}, final:{x:brush.position.x,y:brush.position.y}});
+            brush.move = false;
+        }
+        brush.lastPosition = {x:brush.position.x,y:brush.position.y};
+        setInterval(cicle,1);
+    }
+    cicle();
 })
-document.addEventListener("mouseup", ()=>{
-    isMouseDown = false;
-})
-
-
-canvas.addEventListener("mousemove", (event)=>{
-     if(isMouseDown){
-        console.log(event.pageX + ':' + event.pageY);
-        console.log(event.offsetX + '/' + event.offsetY);
-        ctx.fillRect(event.offsetX/2.7, event.offsetY/3.4, 3, 3);
-     }
-     
-})
-
-
