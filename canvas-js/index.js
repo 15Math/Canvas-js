985ac33e765ad3d933632c6aa4141329046f8d10
15Math <matheus.loureiro1215@gmail.com>
2024-08-21 18:43:54 -0300

    tools buttons done, and bucket parcially done

--- canvas-js/index.js
+++ canvas-js/index.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext('2d', { willReadFrequently: true });
 
     //define tamanho do canvas desenhavel
     canvas.width = 800;
     canvas.height = 500;
 
     //Informações do pincel
+
+    // LIMPAR O CODIGO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     let brush = {
         active: false,
         move: false,
@@ -15,15 +17,22 @@ document.addEventListener('DOMContentLoaded', ()=>{
         color: "#000000",
         lineWidth: 30
     }
+    let eraser = {
+        active: false,
+        color: "#ffffff"
+    }
+    let bucket = {
+        active: false
+    }
 
-
+    let activeTool = brush;
 
     //Desenha a linha
     const draw = (line) => {
         
 
-        lineWidth = brush.lineWidth;
-        ctx.lineWidth = lineWidth;
+    
+        ctx.lineWidth = line.width;
         
         // //Desenha circulo que parece uma linha para suavisar a linha verdadeira
         // ctx.beginPath();
@@ -36,13 +45,20 @@ document.addEventListener('DOMContentLoaded', ()=>{
         ctx.beginPath();
         ctx.moveTo(line.inicial.x, line.inicial.y);
         ctx.lineTo(line.final.x, line.final.y);
-        ctx.strokeStyle = brush.color;
-        ctx.fillOpacity = .0;
+        ctx.strokeStyle = line.color;
         ctx.lineCap = "round";
         ctx.lineJoin = 'round';
         ctx.stroke();
         ctx.closePath();
     }
+
+    // const getActiveTool = ()=>{
+    //     for(a of tools){
+    //         if(a.dataset.active == "true"){
+    //             return a.id;
+    //         }
+    //     }
+    // }
 
     //Toogle da flag de brush ativo
     canvas.onmousedown = ()=> {
@@ -51,9 +67,9 @@ document.addEventListener('DOMContentLoaded', ()=>{
             saveColor();
             saveColorFlag = false;
         }
-        brush.active = true
+        activeTool.active = true;
     };
-    canvas.onmouseup = ()=> brush.active = false;
+    canvas.onmouseup = ()=> activeTool.active = false;
 
     //captura as coordenadas do mouse
     canvas.onmousemove = (event)=>{
@@ -64,9 +80,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     //ciclo que captura o momento de chamar a funçao de desenho
     const cicle = ()=>{
+        //LIMPAR ISSSOOO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         if(brush.active && brush.lastPosition && brush.move){
-            draw({inicial:{x:brush.lastPosition.x,y:brush.lastPosition.y}, final:{x:brush.position.x,y:brush.position.y}});
+            draw({inicial:{x:brush.lastPosition.x,y:brush.lastPosition.y}, final:{x:brush.position.x,y:brush.position.y}, color: brush.color, width: brush.lineWidth});
             brush.move = false;
+        }else if(eraser.active && brush.lastPosition && brush.move){
+            draw({inicial:{x:brush.lastPosition.x,y:brush.lastPosition.y}, final:{x:brush.position.x,y:brush.position.y}, color: eraser.color, width: brush.lineWidth});
+        }else if(bucket.active){
+            floodFill({x:brush.lastPosition, y: brush.lastPosition.y}, hexToRgbaArr(brush.color) );
         }
         brush.lastPosition = {x:brush.position.x,y:brush.position.y};
         requestAnimationFrame(cicle);
@@ -118,7 +139,88 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     const brushTool = document.getElementById('brush');
     const eraserTool = document.getElementById('eraser');
-    const bucketTool = document.getElementById('bucket')
+    const bucketTool = document.getElementById('bucket');
+
+    const tools = [brushTool, eraserTool,  bucketTool]
+
+    const selectTool = (tool)=>{
+        for(a of tools){
+            if(a == tool){
+                a.style.opacity = "1";
+                a.dataset.active = "true";
+            }else{
+                if(a.dataset.active == "true"){
+                    a.style.opacity = ".2";
+                    a.dataset.active = "false";
+                }
+            }
+        }
+    }
+    
+    brushTool.onclick = ()=>{
+        selectTool(brushTool);
+        activeTool.active = false;
+        activeTool = brush;
+    }
+    eraserTool.onclick = ()=>{
+        selectTool(eraserTool);
+        activeTool.active = false;
+        activeTool = eraser;
+    }
+    bucketTool.onclick = ()=>{
+        selectTool(bucketTool);
+        activeTool.active = false;
+        activeTool = bucket;
+    }
+
+    const hexToRgbaArr = (hex, alpha = 1)=>{
+        const r = parseInt(hex.slice(1,3),16);
+        const g = parseInt(hex.slice(3,5),16);
+        const b = parseInt(hex.slice(5,7),16);
+        return [r,g,b,alpha];
+    }
+    const floodFill = (pos, fillColor)=>{
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+        const targetColor = getColor(imageData, pos);
+
+        if(!colorsMatch(targetColor, fillColor)){
+            fillPixel(imageData, pos, targetColor, fillColor );
+        }
+        ctx.putImageData(imageData, 0, 0);
+    }
+
+    const getColor = (imageData, pos)=>{
+        //Lugar no imageData.data em que a cor começa
+        const offSet = (pos.y * imageData.width + pos.x) * 4;
+        //retorna array com o valor da cor
+        return imageData.data.slice(offSet, offSet+4);
+
+    }
+
+    const colorsMatch = (a,b)=>{
+        return a[0] === b[0] && a[1] === b[1] && a[2] === b[2] && a[3] === b[3];
+    }
+
+    const setPixel = (imageData,pos,color) => {
+        const offSet = (pos.y * imageData.width + pos.x) * 4;
+        imageData.data[offSet] = color[0];
+        imageData.data[offSet+1] = color[1];
+        imageData.data[offSet+2] = color[2];
+        imageData.data[offSet+3] = color[3];
+    }
+
+    const fillPixel = (imageData, pos, targetColor, fillColor)=>{
+        if(pos.x < canvas.width && pos.y < canvas.height && pos.x >= 0 && pos.y >= 0){
+            if(colorsMatch(targetColor,getColor(imageData, pos))){
+                setPixel(imageData, pos, fillColor);
+                fillPixel(imageData, {x:pos.x + 1 , y:pos.y}, targetColor, fillColor);
+                fillPixel(imageData, {x:pos.x, y:pos.y + 1}, targetColor, fillColor);
+                fillPixel(imageData, {x:pos.x - 1 , y:pos.y}, targetColor, fillColor);
+                fillPixel(imageData, {x:pos.x , y:pos.y - 1}, targetColor, fillColor);
+            }
+        }
+    }
 
 })
 
